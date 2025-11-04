139eada580ea63e8f4899d28456380a37779af88
kochet94 <kochet94@mail.ru>
2025-11-04 22:37:54 +0300

    added requirements.txt

--- tests/test_qa_cells_drc.py
+++ tests/test_qa_cells_drc.py
@@ -1,41 +1,12 @@
 __unittest = True
 
 import unittest
+import subprocess
 
-import sys
-import os
+from pathlib import Path
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
-from lib.mzi import mzi
-
-class TestMziOpticalPortsSanity(unittest.TestCase):
-
-    EXPECTED_PORTS = {
-        'o1': {
-            'type' : 'optical',
-            'orientation' : 180.0
-        },
-        'o2': {
-            'type' : 'optical',
-            'orientation' : 180.0
-        },
-        'o3': {
-            'type' : 'optical',
-            'orientation' : 0.0
-        },
-        'o4': {
-            'type' : 'optical',
-            'orientation' : 0.0
-        }
-    }
+class TestQaCellsDrc(unittest.TestCase):
 
     def test_ports_presence(self):
 
-        mzi_component = mzi()
-        ports_filtered = filter(lambda x: x.port_type != 'electrical', mzi_component.ports)
-
-        for port in ports_filtered:
-            self.assertIn(port.name, self.EXPECTED_PORTS.keys())
-            self.assertEqual(port.port_type, self.EXPECTED_PORTS[port.name]['type'])
-            self.assertEqual(port.orientation, self.EXPECTED_PORTS[port.name]['orientation'])
+        keep_out_heater_drc = Path()
