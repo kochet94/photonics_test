e4d6d199d05921e2090ac3be39595d440e540125
kochet94 <kochet94@mail.ru>
2025-11-04 22:56:28 +0300

    added qa cells test

--- tests/test_qa_cells_drc.py
+++ tests/test_qa_cells_drc.py
@@ -2,11 +2,36 @@ __unittest = True
 
 import unittest
 import subprocess
+import shutil
 
 from pathlib import Path
 
 class TestQaCellsDrc(unittest.TestCase):
 
-    def test_ports_presence(self):
+    TEST_DATA_DIR = Path(__file__).parent / 'test_data'
 
-        keep_out_heater_drc = Path()
+    def setUp(self):
+        if self.TEST_DATA_DIR.exists():
+           shutil.rmtree(self.TEST_DATA_DIR) 
+        self.TEST_DATA_DIR.mkdir()
+        
+    def test_keepout_heater_drc(self):
+
+        keepout_heater_drc = Path(__file__).parents[2] / 'drc' / 'KEEP_OUT_HEATER.lydrc'
+        keepout_heater_qa_cell = Path(__file__).parents[2] / 'qa_cells' / 'HEATER_PADDING' / 'QA_HEATER_PADDING.gds'
+        report_file_path = keepout_heater_drc / 'keepout_heater_drc_report.lyrdb'
+
+        sp_args = [
+            'klayout_app',
+            '-b',
+            '-r',
+            keepout_heater_drc.as_posix(),
+            '-rd',
+            f'input_layout={keepout_heater_qa_cell.as_posix()}',
+            '-rd',
+            f'output_report={report_file_path.as_posix()}'
+        ]
+
+        sp = subprocess.run(sp_args)
+
+        self.assertEqual(sp.returncode, 1)
--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,27 @@
 import unittest
 
-from tests.test_mzi_optical_ports_sanity import TestMziPortsSanity
+from test_mzi_optical_ports_sanity import TestMziOpticalPortsSanity
+from test_qa_cells_drc import TestQaCellsDrc
 
 
 if __name__ == '__main__':
 
     failures = 0
 
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestMziPortsSanity)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMziOpticalPortsSanity)
     runner = unittest.TextTestRunner(verbosity=2)
 
     print('\n')
     print('MZI PORTS SANITY SECTION')
+    print(70 * '-')
+
+    failures += len(runner.run(suite).failures)
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestQaCellsDrc)
+    runner = unittest.TextTestRunner(verbosity=2)
+
+    print('\n')
+    print('QA CELLS DRC SECTION')
     print(70 * '-')
 
     failures += len(runner.run(suite).failures)
